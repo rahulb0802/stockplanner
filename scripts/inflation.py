6a6922ff8a8558c7ff42371fca99d3e5726bc08b
rahulb08 <rahulsai.billakanti11@gmail.com>
2024-12-06 19:44:14 -0600

    stuff

--- scripts/inflation.py
+++
@@ -1,9 +0,0 @@
-from fredapi import Fred
-
-fred = Fred(api_key='6827b33b1899d3a7996eaee1ae920bbd')
-nigeria_inflation = fred.get_series('FPCPITOTLZGNGA')
-us_inflation = fred.get_series('FPCPITOTLZGUSA')
-nigeria_inflation.loc['2024-01-01'] = 31.7
-us_inflation.loc['2024-01-01'] = 3.0
-print(nigeria_inflation)
-print(us_inflation)
